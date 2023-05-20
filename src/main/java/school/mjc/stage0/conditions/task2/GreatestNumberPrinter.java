1466073d6512badda79cc1ec21fae3920df04b3c
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-20 16:36:39 +0600

    Update GreatestNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+            if(first > second) {
+            System.out.println(first);
+            } else if(second > first) {
+            System.out.println(second);
+            } else {
+            System.out.println(first);
+            System.out.println(second);
+            }
     }
 }
